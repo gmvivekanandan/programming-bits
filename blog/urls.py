c82e5b23c2f698cb9c7f450eb6140b1a614cb0f7
Vivekanandan GM <joker12345.rm@gmail.com>
2021-01-15 21:40:51 +0530

    n

--- blog/urls.py
+++ blog/urls.py
@@ -24,7 +24,3 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('blogs.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
