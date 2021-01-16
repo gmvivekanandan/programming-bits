48fbaae0c1d28babb147f3b1ebdf4b725ab43236
Vivekanandan GM <joker12345.rm@gmail.com>
2021-01-16 10:05:17 +0530

    secret

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e)=c4ae+om#3g97)-%d9$m0x4@g3&+z9$%44v(o=ihy1esf*qn'
+SECRET_KEY = 'CHANGE_ME!!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
