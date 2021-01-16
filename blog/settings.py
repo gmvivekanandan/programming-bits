20c3a0b4bfc6b5d405aabd30058e2070c9d0ced4
Vivekanandan GM <joker12345.rm@gmail.com>
2021-01-16 11:26:52 +0530

    secret

--- blog/settings.py
+++ blog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'CHANGE_ME!!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
